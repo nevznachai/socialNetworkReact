8217d0ccb25ee81d5850ae9a403f7a1d966c5626
nevznachai <104390336+nevznachai@users.noreply.github.com>
2022-12-22 19:24:56 +0300

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,10 @@
+import openGoogle from './google.js';
+import openYandex from './yandex.js';
+
+let OpenAllMaps = () => {
+
+    openGoogle();
+    openYandex();
+    
+}
+export default OpenAllMaps;
